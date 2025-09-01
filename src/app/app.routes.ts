d209217a249e35c38b91912eb7b3a99a491b88a9
farhan1807ahmad <fa9711173@gmail.com>
2025-09-01 12:54:13 +0530

    second commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Layout } from './pages/layout/layout';
+import { Employee } from './pages/employee/employee';
+import { Dashboard } from './pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: Login
+    },
+    {
+        path: '',
+        component: Layout,
+        children: [
+            {   
+              path: 'dashboard',
+              component: Dashboard
+            },
+            {
+              path: 'employee',
+              component: Employee
+            },
+            {
+              path: 'leaves',
+              component: Employee
+            }
+        ]
+    }
+];
